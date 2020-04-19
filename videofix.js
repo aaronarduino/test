527affa85848fd8398c53935ec0d61734c2d2e7a
Aaron <aaronarduino@users.noreply.github.com>
2020-04-18 21:27:05 -0500

    Update videofix.js

--- videofix.js
+++ videofix.js
@@ -1,4 +1,10 @@
 console.log("Hello!");
+document.onLoad = function() {
+    var contentDiv = document.getElementById('content');
+    var sty = document.createElement("style");
+    sty.innerHTML = ".hcc-livestream-player { padding-bottom: none; } .hcc-livestream-player div { padding-bottom: none; } .hcc-livestream-player div div { padding-bottom: none; }";
+    contentDiv.appendChild(sty);
+};
 var ancestor = document.getElementById('la1-videoContainer');
 while (typeof ancestor === 'undefined'){
     ancestor = document.getElementById('la1-videoContainer');
