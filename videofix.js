4ffe34ef8b756757cdc3007a1590929f2a6e7a6e
Aaron <aaronarduino@users.noreply.github.com>
2020-04-18 21:04:25 -0500

    Update videofix.js

--- videofix.js
+++ videofix.js
@@ -1,15 +1,13 @@
-document.onload = function() {
-    console.log("Hello!");
-    var ancestor = document.getElementById('la1-videoContainer');
-    while (typeof ancestor === 'undefined'){
-        ancestor = document.getElementById('la1-videoContainer');
-    }
-    var descendents = ancestor.getElementsByTagName('*');
+console.log("Hello!");
+var ancestor = document.getElementById('la1-videoContainer');
+while (typeof ancestor === 'undefined'){
+    ancestor = document.getElementById('la1-videoContainer');
+}
+var descendents = ancestor.getElementsByTagName('*');
 
-    var i, e, d;
-    for (i = 0; i < descendents.length; ++i) {
-        e = descendents[i];
-        console.log(e.className);
-        //e.removeAttribute('style');
-    }
-};
+var i, e, d;
+for (i = 0; i < descendents.length; ++i) {
+    e = descendents[i];
+    console.log(e.className);
+    //e.removeAttribute('style');
+}
